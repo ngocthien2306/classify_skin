a0f109e5ba68f326954c42544d0999855fd09d3d
Ngoc Thien <ngocthien.dev23@gmail.com>
2025-01-05 01:21:58 +0800

    update code

--- scripts/data/split_data.py
+++ scripts/data/split_data.py
@@ -1,7 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(PROJECT_ROOT)
 import yaml
 import pandas as pd
 import matplotlib.pyplot as plt
